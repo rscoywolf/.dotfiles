943c02eec964d5911fd4b7e65c5c9cc67ca820c5
rscoywolf <rscoywolf@gmail.com>
2023-03-25 11:39:42 -0400

    awdswadS

--- .scripts/link_bin.sh
+++ .scripts/link_bin.sh
@@ -4,9 +4,10 @@ for file in ~/.dotfiles/.scripts/.bin/*; do
     filename=$(basename "$file")
     target_file=/bin/"$filename"
     if [[ -e "$target_file" && ! -L "$target_file" ]]; then
-        mv "$target_file" "${target_file}_bkup"
+        sudo mv "$target_file" "${target_file}_bkup"
     elif [[ -L "$target_file" ]]; then
-        rm "$target_file"
+        sudo rm "$target_file"
     fi
-    ln -s "$file" "$target_file"
+    chmod +x "$file"
+    sudo ln -s "$file" "$target_file"
 done
--- .scripts/set_up.sh
+++ .scripts/set_up.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 # install the packages
-sudo pacman -S i3 nvim feh fish redshift firefox signal-desktop discord neofetch fzf
+sudo pacman -S i3
+sudo pacman -S neovim
+sudo pacman -S feh
+sudo pacman -S fish
+sudo pacman -S redshift
+sudo pacman -S firefox
+sudo pacman -S signal-desktop
+sudo pacman -S discord
+sudo pacman -S neofetch
+sudo pacman -S fzf
+
+# install snap
+cd ~/Downloads
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+# install snap apps
+sudo snap install --classic code
 
 # run the script called link_configs.sh
 chmod +x link_configs.sh
@@ -10,3 +30,12 @@ chmod +x link_configs.sh
 # put scripts in bin
 chmod +x link_bin.sh
 (./link_bin.sh)
+
+
+# git
+git config --global user.email "rscoywolf@gmail.com"
+git config --global user.name "rscoywolf"
+git config credential.helper store
+
+cd
+git clone https://github.com/rscoywolf/schoolwork.git
