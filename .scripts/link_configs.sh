e7f9013752a3897e2c4b4a5c20fda7c338c9e22a
rscoywolf <rscoywolf@gmail.com>
2023-03-25 12:11:17 -0400

    fddsfsdf

--- .scripts/link_configs.sh
+++ .scripts/link_configs.sh
@@ -4,7 +4,18 @@ for file in ~/.dotfiles/.config/*; do
     filename=$(basename "$file")
     target_file=~/.config/"$filename"
     if [[ -e "$target_file" && ! -L "$target_file" ]]; then
-        mv "$target_file" "${target_file}_bkup"
+        mv ">$target_file" "${target_file}_bkup"
+    elif [[ -L "$target_file" ]]; then
+        rm "$target_file"
+    fi
+    ln -s "$file" "$target_file"
+done
+
+for file in ~/.dotfiles/.homeconfig/; do
+    filename=$(basename "$file")
+    target_file=~/"$filename"
+    if [[ -e "$target_file" && ! -L "$target_file" ]]; then
+        mv ">$target_file" "${target_file}_bkup"
     elif [[ -L "$target_file" ]]; then
         rm "$target_file"
     fi
