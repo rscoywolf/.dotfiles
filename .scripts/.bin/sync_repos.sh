d925ac35e0d302a592a1089172ff1f12058aa9fa
rscoywolf <rscoywolf@gmail.com>
2023-03-27 22:47:53 -0400

    auto commit

---
+++ .scripts/.bin/sync_repos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ~/.dotfiles/
+git pull
+git add .
+git commit -m "auto commit"
+git push
+
+if [ -d ~/schoolwork ]; then
+    cd ~/schoolwork
+    git pull
+    git add .
+    git commit -m "auto commit"
+    git push
+fi
