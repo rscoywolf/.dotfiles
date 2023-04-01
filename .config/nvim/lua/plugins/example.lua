2343e6f6e7ba36ac338a289be81ae305071bd522
rscoywolf <rscoywolf@gmail.com>
2023-04-01 19:20:00 -0400

    successfully installed vimtex?

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end
 
 -- every spec file under config.plugins will be loaded automatically by lazy.nvim
 --
@@ -9,6 +9,9 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  -- add vimtex
+  { "lervag/vimtex" },
+
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
