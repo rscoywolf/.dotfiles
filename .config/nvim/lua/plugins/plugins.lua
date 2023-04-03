722006731fd2392a166e1f1e6c18151edd41ddf1
rscoywolf <rscoywolf@gmail.com>
2023-04-03 18:45:20 -0400

    fix error message for unavailable parser in treesitter installation

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -14,4 +14,21 @@ return {
   {
     "khaveesh/vim-fish-syntax",
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ignore_install = { "help" }
+
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "dockerfile",
+          "git_config",
+          "jsdoc",
+          "make",
+          "toml",
+          "vimdoc",
+        })
+      end
+    end,
+  },
 }
