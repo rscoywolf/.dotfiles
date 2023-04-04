e333e19c695271cbc86ccadc46203d62eef081d3
rscoywolf <rscoywolf@gmail.com>
2023-04-04 15:43:38 -0400

    trying to get polybar working with battery

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
@@ -14,16 +14,21 @@ num_monitors=$(xrandr --query | grep " connected" | wc -l)
 monitor1=$(xrandr --query | grep " connected" | head -n 1 | cut -d " " -f 1)
 monitor2=$(xrandr --query | grep " connected" | tail -n 1 | cut -d " " -f 1)
 
-# Set the monitor setting for each bar
-sed -i "/^\[bar\/bar1\]/,/^\[bar\/bar2\]/ s/^\(monitor\s*=\s*\).*$/\1$monitor1/" ~/.config/polybar/config.ini
-sed -i "/^\[bar\/bar2\]/,/^\[module\// s/^\(monitor\s*=\s*\).*$/\1$monitor2/" ~/.config/polybar/config.ini
+display_values_path="$HOME/.display_values.conf"
+touch "$display_values_path"
+echo "[display]" >"$display_values_path"
+echo "monitor1 = $monitor1" >>"$display_values_path"
+echo "monitor2 = $monitor2" >>"$display_values_path"
 
 # Launch the appropriate number of bars
 if [ "$num_monitors" -eq 1 ]; then
-    polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+	polybar bar1 2>&1 | tee -a /tmp/polybar1.log &
+	disown
 elif [ "$num_monitors" -eq 2 ]; then
-    polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-    polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+	polybar bar1 2>&1 | tee -a /tmp/polybar1.log &
+	disown
+	polybar bar2 2>&1 | tee -a /tmp/polybar2.log &
+	disown
 fi
 
 echo "Bars launched..."
