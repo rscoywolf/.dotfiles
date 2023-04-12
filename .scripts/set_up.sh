b508ddf1dab84a56890c4dedca177731646d20df
rscoywolf <rscoywolf@gmail.com>
2023-04-12 14:13:33 -0400

    remove snap version of vs code before installing from official repo

--- .scripts/set_up.sh
+++ .scripts/set_up.sh
@@ -95,6 +95,9 @@ function install_snap() {
 		read -p "Install VS Code and Teams for Linux? [y/N]: " choice_snap_apps
 		if [[ "$choice_snap_apps" =~ ^[Yy]$ ]]; then
 			cd ~
+			rm -rf ~/.config/Code
+			rm -rf ~/.vscode/
+			sudo snap remove code
 			git clone https://aur.archlinux.org/visual-studio-code-bin.git
 			cd visual-studio-code-bin
 			makepkg -si
